34d81a1b5e7c4628d224e17ec0ccfca83271487f
Andrew <andreicsgoawp@gmail.com>
2025-09-02 17:47:52 +0300

    Added basic objects functions

--- 250831_supermario/main.cpp
+++ 250831_supermario/main.cpp
@@ -1,29 +1,50 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define mapWidth 80
-#define mapHeight 25
+#include <math.h>
+
+#define mapWidth 120
+#define mapHeight 30
+
+typedef struct SObject {
+    float x, y;
+} TObject;
 
 char map[mapHeight][mapWidth + 1];
+TObject mario;
 
 void ClearMap() {
-    for (int y = 0; y < mapHeight; y++) {
-        map[y][0] = '#';
-        for (int x = 1; x < mapWidth; x++) {
-            map[y][x] = '.';
-        }
-        map[y][mapWidth - 1] = '#';
+    for (int i = 0; i < mapWidth; i++) {
+        map[0][i] = '.';
+    }
+    map[0][mapWidth] = '\0';
+    for (int j = 0; j < mapHeight; j++) {
+        sprintf(map[j], map[0]);
     }
 }
 
 void PrintMap() {
-    for (int y = 0; y < mapHeight; y++) {
-        map[y][mapWidth] = '\0'; // Null-terminate each row
-        std::cout << map[y] << std::endl;
+    map[mapHeight - 1][mapWidth - 1] = '\0';
+    for (int j = 0; j < mapHeight; j++) {
+        printf("%s", map[j]);
     }
 }
 
+void SetObjectPos(TObject* obj, float xPos, float yPos) {
+    (*obj).x = xPos;
+    (*obj).y = yPos;
+}
+
+void PlaceObject(TObject obj) {
+    int ix = (int)round(obj.x);
+    int iy = (int)round(obj.y);
+    map[iy][ix] = 'M';
+}
+
 int main() {
+    SetObjectPos(&mario, 20, 10);
     ClearMap();
+    PlaceObject(mario);
     PrintMap();
     return 0;
 }
