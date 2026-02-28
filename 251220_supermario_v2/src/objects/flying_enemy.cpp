92dd9fcfed2338f4d7324c316a50135810881e1a
Andrew <aandreyt7@gmail.com>
2026-02-28 17:40:49 +0300

    Supermario_v2: removed flying enemy inherence to enemy

--- 251220_supermario_v2/src/objects/flying_enemy.cpp
+++ 251220_supermario_v2/src/objects/flying_enemy.cpp
@@ -1,20 +1,55 @@
 #include "flying_enemy.hpp"
 
+#include "map_movable.hpp"
+
 #include <cmath>
 
 using biv::FlyingEnemy;
 
-FlyingEnemy::FlyingEnemy(
-	const Coord& top_left,
-	const int width,
-	const int height,
-	const float fly_amplitude
-) : Enemy(top_left, width, height),
-	base_y(top_left.y),
-	fly_amplitude(fly_amplitude) {
-
+FlyingEnemy::FlyingEnemy(const Coord& top_left, const int width, const int height, const float fly_amplitude) 
+	: RectMapMovableAdapter(top_left, width, height) {
 	hspeed = 0.15f;
 	vspeed = 0;
+
+}
+
+biv::Rect FlyingEnemy::get_rect() const noexcept {
+	return {top_left, width, height};
+}
+
+biv::Speed FlyingEnemy::get_speed() const noexcept {
+	return {vspeed, hspeed};
+}
+
+void FlyingEnemy::process_horizontal_static_collision(Rect* obj) noexcept {
+	hspeed = -hspeed;
+	move_horizontally();
+}
+
+void FlyingEnemy::process_mario_collision(Collisionable* mario) noexcept {
+	if (mario->get_speed().v > 0 && mario->get_speed().v != V_ACCELERATION) {
+		kill();
+	} else {
+		mario->kill();
+	}
+}
+
+void FlyingEnemy::process_vertical_static_collision(Rect* obj) noexcept {
+	// Проверка: не свалился ли враг с корабля. 
+	// Т.е., если он на краю, то он должен разверуться 
+	// и побежать в обратную сторону.
+	top_left.x += hspeed;
+	if (!has_collision(obj)) {
+		process_horizontal_static_collision(obj);
+	} else {
+		top_left.x -= hspeed;
+	}
+	
+	// Особенность модели вертикального передвижения в игре.
+	if (vspeed > 0) {
+		top_left.y -= vspeed;
+		vspeed = 0;
+	}
 }
 
 void FlyingEnemy::move_vertically() noexcept {
@@ -23,7 +58,6 @@ void FlyingEnemy::move_vertically() noexcept {
 		fly_phase -= 2 * M_PI;
 	}
 	top_left.y = base_y + fly_amplitude * std::sin(fly_phase);
-}
 
-void FlyingEnemy::process_vertical_static_collision(Rect* obj) noexcept {
+	biv::Movable::move_vertically();
 }
--- 251220_supermario_v2/src/objects/flying_enemy.hpp
+++ 251220_supermario_v2/src/objects/flying_enemy.hpp
@@ -1,15 +1,18 @@
 #pragma once
 
-#include "enemy.hpp"
+#include "collisionable.hpp"
+#include "movable.hpp"
+#include "rect.hpp"
+#include "rect_map_movable_adapter.hpp"
+#include "speed.hpp"
 
 namespace biv {
-	class FlyingEnemy : public Enemy {
+	class FlyingEnemy : public RectMapMovableAdapter, public Movable, public Collisionable {
 		private:
 			float base_y;
 			float fly_amplitude;
-			float fly_phase = 0;
+			float fly_phase;
 			const float fly_speed = 0.02f;
-
 		public:
 			FlyingEnemy(
 				const Coord& top_left,
@@ -18,7 +21,12 @@ namespace biv {
 				const float fly_amplitude = 5.0f
 			);
 
+			Rect get_rect() const noexcept override;
+			Speed get_speed() const noexcept override;
+
+			void process_horizontal_static_collision(Rect*) noexcept override;
+			void process_mario_collision(Collisionable*) noexcept override;
+			void process_vertical_static_collision(Rect*) noexcept override;
 			void move_vertically() noexcept override;
-			void process_vertical_static_collision(Rect* obj) noexcept override;
 	};
 }
