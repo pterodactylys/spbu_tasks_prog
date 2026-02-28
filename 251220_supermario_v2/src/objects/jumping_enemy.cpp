215a946719808270a30a89b3356c9d008df91c79
Andrew <aandreyt7@gmail.com>
2026-02-28 17:15:39 +0300

    Supermario_v2: removed jumping enemy inheritance to enemy class

--- 251220_supermario_v2/src/objects/jumping_enemy.cpp
+++ 251220_supermario_v2/src/objects/jumping_enemy.cpp
@@ -1,31 +1,34 @@
 #include "jumping_enemy.hpp"
 
+#include "map_movable.hpp"
+
 using biv::JumpingEnemy;
 
-JumpingEnemy::JumpingEnemy(
-    const Coord& top_left,
-    const int width,
-    const int height,
-    const int jump_interval
-) : Enemy(top_left, width, height),
-    time_to_jump(jump_interval),
-    jump_interval(jump_interval) {
+JumpingEnemy::JumpingEnemy(const Coord& top_left, const int width, const int height) 
+	: RectMapMovableAdapter(top_left, width, height) {
+	vspeed = 0;
+	hspeed = 0.2;
+}
 
-    hspeed = 0;
-    }
+biv::Rect JumpingEnemy::get_rect() const noexcept {
+	return {top_left, width, height};
+}
 
-void JumpingEnemy::move_vertically() noexcept {
-    if (!vspeed) {
-        if (time_to_jump <= 0) {
-            vspeed = JUMP_SPEED;
-            time_to_jump = jump_interval;
-        }
-        else {
-            time_to_jump--;
-        }
-    }
+biv::Speed JumpingEnemy::get_speed() const noexcept {
+	return {vspeed, hspeed};
+}
 
-    biv::Movable::move_vertically();
+void JumpingEnemy::process_horizontal_static_collision(Rect* obj) noexcept {
+	hspeed = -hspeed;
+	move_horizontally();
+}
+
+void JumpingEnemy::process_mario_collision(Collisionable* mario) noexcept {
+	if (mario->get_speed().v > 0 && mario->get_speed().v != V_ACCELERATION) {
+		kill();
+	} else {
+		mario->kill();
+	}
 }
 
 void JumpingEnemy::process_vertical_static_collision(Rect* obj) noexcept {
@@ -41,4 +44,18 @@ void JumpingEnemy::process_vertical_static_collision(Rect* obj) noexcept {
         top_left.y -= vspeed;
         vspeed = 0;
     }
+}
+
+void JumpingEnemy::move_vertically() noexcept {
+    if (!vspeed) {
+        if (time_to_jump <= 0) {
+            vspeed = JUMP_SPEED;
+            time_to_jump = jump_interval;
+        }
+        else {
+            time_to_jump--;
+        }
+    }
+
+    biv::Movable::move_vertically();
 }
--- 251220_supermario_v2/src/objects/jumping_enemy.hpp
+++ 251220_supermario_v2/src/objects/jumping_enemy.hpp
@@ -1,21 +1,25 @@
 #pragma once
 
-#include "enemy.hpp"
+#include "collisionable.hpp"
+#include "movable.hpp"
+#include "rect.hpp"
+#include "rect_map_movable_adapter.hpp"
+#include "speed.hpp"
 
 namespace biv {
-    class JumpingEnemy : public Enemy {
+	class JumpingEnemy : public RectMapMovableAdapter, public Movable, public Collisionable {
         private:
             int time_to_jump = 0;
             int jump_interval = 50;
-        public:
-            JumpingEnemy(
-                const Coord& top_left,
-                const int width,
-                const int height,
-                const int jump_interval = 50
-            );
+		public:
+			JumpingEnemy(const Coord& top_left, const int width, const int height);
 
-            void move_vertically() noexcept override;
-            void process_vertical_static_collision(Rect* obj) noexcept override;
-    };
+			Rect get_rect() const noexcept override;
+			Speed get_speed() const noexcept override;
+
+			void process_horizontal_static_collision(Rect*) noexcept override;
+			void process_mario_collision(Collisionable*) noexcept override;
+			void process_vertical_static_collision(Rect*) noexcept override;
+            void move_vertically() noexcept;
+	};
 }
